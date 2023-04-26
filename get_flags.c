095acf82f96f50c9f7a0cebceb410cab40d3a67f
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-25 18:11:47 -0700

    Tunning and adding comments

--- get_flags.c
+++ get_flags.c
@@ -1,33 +1,38 @@
 #include "main.h"
+
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
- */
+* get_flags - Detect active flags
+* @format: Formatted string in which to print the arguments
+* @i: Current index
+* Return:
+*	0	 = none	: print normally (right justify, space fill)
+*	F_MINUS  = -    : left justify
+*	F_ZERO   = 0    : leading zero fill
+*	F_PLUS   = +    : print + on positive numbers
+*	F_SPACE  =      : invisible + sign
+*	F_HASH	 = #	: #
+*/
 int get_flags(const char *format, int *i)
 {
-/* - + 0 # ' ' */
-/* 1 2 4 8  16 */
-int j, curr_i;
-int flags = 0;
-const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	int j, current_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-{
-for (j = 0; FLAGS_CH[j] != '\0'; j++)
-if (format[curr_i] == FLAGS_CH[j])
-{
-flags |= FLAGS_ARR[j];
-break;
-}
+	for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[current_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
 
-if (FLAGS_CH[j] == 0)
-break;
-}
+			if (FLAGS_CH[j] == 0)
+			break;
+	}
 
-*i = curr_i - 1;
+	*i = current_i - 1;
 
-return (flags);
+	return (flags);
 }
