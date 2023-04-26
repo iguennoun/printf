1547da8c8555a74fd5eef20ba25e01ff733f9f9b
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-25 18:35:54 -0700

    Correcting betty style + adding comment

--- get_width.c
+++ get_width.c
@@ -1,36 +1,39 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: width.
- */
+* get_width - Get the specified width when printing numbers
+*		It's usefull in printing tables
+*		because small and large number both took
+*		the same amount of space
+* @format: The string in which to print the arguments
+* @i: Nbr of arguments to be printed
+* @list: List of arguments
+*
+* Return: integer of the specified with or 0
+*/
 int get_width(const char *format, int *i, va_list list)
 {
-int curr_i;
-int width = 0;
+	int current_i;
+	int width = 0;
 
-for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-{
-if (is_digit(format[curr_i]))
-{
-width *= 10;
-width += format[curr_i] - '0';
-}
-else if (format[curr_i] == '*')
-{
-curr_i++;
-width = va_arg(list, int);
-break;
-}
-else
-break;
-}
+	for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
+	{
+		if (is_digit(format[current_i]))
+		{
+			width *= 10;
+			width += format[current_i] - '0';
+		}
+		else if (format[current_i] == '*')
+		{
+			current_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-*i = curr_i - 1;
+	*i = current_i - 1;
 
-return (width);
+	return (width);
 }
