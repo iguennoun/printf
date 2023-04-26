547266a94619f98e50654fc6e446098cdf58e948
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-04-25 18:26:03 -0700

    Adding num specification + comment

--- get_size.c
+++ get_size.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
- * Return: Precision.
- */
+* get_size - Get the size of a numeric : Integer || Short || Long
+* @format: The string in which to print the arguments
+* @i: Nbr of arguments to be printed
+*
+* Return: The size of a numeric 0 or S_long or S_SHORT
+*/
 int get_size(const char *format, int *i)
 {
-int curr_i = *i + 1;
-int size = 0;
+	int current_i = *i + 1;
+	int size = 0;
 
-if (format[curr_i] == 'l')
-size = S_LONG;
-else if (format[curr_i] == 'h')
-size = S_SHORT;
+	if (format[current_i] == 'l')
+		size = S_LONG;
+	else if (format[current_i] == 'h')
+		size = S_SHORT;
 
-if (size == 0)
-*i = curr_i - 1;
-else
-*i = curr_i;
+	if (size == 0)
+		*i = current_i - 1;
+	else
+		*i = current_i;
 
-return (size);
+	return (size);
 }
